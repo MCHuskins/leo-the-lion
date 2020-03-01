e43fbcaaf2c2750dcb0228b92982cdac32272694
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-02-29 17:40:57 -0800

    fixed some of the problems

--- lion/Assets/code/player.cs
+++ lion/Assets/code/player.cs
@@ -9,6 +9,9 @@ public class player : MonoBehaviour
     public int jump = 0;
     //Leo look at the code it is hard to explane
     public int jumpmax = 1000;
+    //can jump
+    //public Transform ground;
+    //private bool canjump = false;
     //Leo jumpi is jump increments
     public int jumpi = 30;
     void Start()
@@ -19,12 +22,14 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //left or right
         if (Input.GetKey (KeyCode.A)) {
         transform.Translate (Vector3.left*speed*Time.deltaTime);
     }
     if(Input.GetKey (KeyCode.D)) {
         transform.Translate (Vector3. right*speed*Time.deltaTime);
     }
+    //jump
     if (Input.GetKey(KeyCode.Space)) {
         if(jump<= jumpmax){
             jump = jump+ jumpi;}
@@ -32,6 +37,7 @@ public class player : MonoBehaviour
     if(jump>=jumpi && !(Input.GetKey(KeyCode.Space))){
         transform.Translate (Vector3. up*jumpi*Time.deltaTime);
         jump = jump - jumpi;
+        //canjump = false;
     }
-    }
+}
 }
