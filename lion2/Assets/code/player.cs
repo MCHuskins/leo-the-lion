8c0ac6ce4292cb999ac0322805a6e014c1777247
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-03-05 21:22:27 -0800

    jump is know working

--- lion2/Assets/code/player.cs
+++ lion2/Assets/code/player.cs
@@ -7,18 +7,29 @@ public class player : MonoBehaviour
     //player spped and jump
     public int speed = 10;
     public float jump = 0.0f;
-    //jump
-    private bool canjump1 = false;
-    private bool canjump2 = false;
-    //Leo jumphight is jump increments
-    public float jumphight = 20f;
-    public float jumpw = 2f;
-    public float jumpn = 10f;
+    // can jump for the charge
+    public bool canjump = false;
+    public bool canjump1 = false;
+    public bool canjump2 = false;
+    //Leo jump veribales
+    public float jumphight = 5f;
+    public float jumpdown = 2f;
+    public Rigidbody rb;
+    //charge
+    public float jumpw = 1f;
+    public float jumpn = 2f;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+    }
+    //ground?
+void OnCollisionEnter(Collision col){
+        canjump = true;
     }
 
+void OnCollisionExit (Collision col){
+        canjump = false;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -30,22 +41,25 @@ public class player : MonoBehaviour
         transform.Translate (Vector3. right*speed*Time.deltaTime);
     }
     //jump charge
-    if (Input.GetKey(KeyCode.Space)) {
+    if (Input.GetKey(KeyCode.Space) && canjump) {
         canjump1 = true;
         jump = (jump + (jumpw*Time.deltaTime));
         if (canjump1 && jump>=jumpn){
             canjump2 = true;
         }
         }
-    //this is going up leo
+    //jump things:
+    // if (rb.velocity.y < 0) {
+    //     rb.velocity = Vector3.up*Physics2D.gravity*jumpdown*Time.deltaTime;
+    // }
     if(canjump1 && !(Input.GetKey(KeyCode.Space))){
-        transform.Translate(Vector3.up * jumphight * Time.deltaTime, Space.World);
+        rb.velocity = new Vector3(0, jumphight+2, 0);
         canjump1 = false;
         jump = 0;
     }
     //set thing back to 0 this was need to fix the jump
     if(canjump2 && !(Input.GetKey(KeyCode.Space))){
-        transform.Translate (Vector3.up*jumphight*Time.deltaTime);
+        rb.velocity = new Vector3(0, jumphight+jumphight, 0);
         canjump2 = false;
         jump = 0;
     }
