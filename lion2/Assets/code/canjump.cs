23cfebc208c47170e1547190d30e53ce3cd90a8b
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-03-10 12:14:32 -0700

    PASS

---
+++ lion2/Assets/code/canjump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canjump : MonoBehaviour
+{
+    // public static int jumpi; //how high a player can jump
+    // Text text;
+    // void Start()
+    // {
+    //     text = GetComponent<Text>();
+    //     jumpi = 0;
+    // }
+
+    // void Update()
+    // {
+    //     text.text = "Jump hight: "+ jumpi;
+    // }
+public int jumptest = 1;
+    void OnGUI()
+    {
+        GUILayout.Label("score"+jumptest);;
+    }
+}
