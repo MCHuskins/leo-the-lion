1664a325e62002626153edc361509cb8200501c8
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-03-13 14:29:05 -0700

    art is a lot better

---
+++ lion2/Assets/code/walk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+//player current size
+// public Vector2 playerS;
+// public float test = 0.02f;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKey (KeyCode.D)) {
+            transform.localScale = new Vector2(1,1);
+        }
+
+        if(Input.GetKey (KeyCode.A)) {
+            transform.localScale = new Vector2(-1,1);
+        }
+    }
+}
