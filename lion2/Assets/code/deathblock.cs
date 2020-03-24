d29527269134a0b8b3dc53beb50f80929228c503
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-03-23 22:24:12 -0700

    sniper works now

---
+++ lion2/Assets/code/deathblock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathblock : MonoBehaviour
+{
+    //move player back
+    void OnCollisionEnter2D(Collision2D col){
+            if(col.gameObject.tag == "Player"){
+                col.transform.position = new Vector2(0,0);
+            }
+            }
+}
--- lion2/Assets/code/sniper.cs
+++ lion2/Assets/code/sniper.cs
@@ -5,21 +5,75 @@ using UnityEngine;
 public class sniper : MonoBehaviour
 {
 //follow
-public Vector2 playerp;
+public Transform player;
 public bool follow = true;
 public int followspeed= 18;
+public float shouldf = 0f;
+public bool onplayer = false;
+//right before shooting
+public float swait = 0f;
+//time betwwen shots
+public int interval = 1;
 
-public Rigidbody2D rb;
-public Transform player;
+//shooting
+public bool shooting = false;
+//after shot
+public float ashott = 0f;
+public int tilnext = 15;
+public bool justshot = false;
+    void Start(){
 
-    void Start()
-    {
-    rb = GetComponent<Rigidbody2D>();
+    }
+    void OnTriggerStay2D(Collider2D col){
+        if (col.gameObject.tag == "Player") {
+            onplayer = true;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.gameObject.tag == "Player") {
+            onplayer = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col){
+        if(col.gameObject.tag == "Player"){
+
+            onplayer = false;
+        }
     }
 
-    void Update()
-    {
+    void Update(){
         //follow section
+        // isonplayer();
+        if(onplayer){
+            shouldf += 20*Time.deltaTime;
+        }
+        if(!onplayer && !(shouldf>=interval)){
+            shouldf = 0;
+        }
+        if(shouldf>=interval){
+            swait += 20*Time.deltaTime;
+            follow = false;
+        }
+        if(swait>= interval){
+            shooting = true;
+            shouldf = 0;
+            swait = 0;
+        }
+        if (shooting){
+            if(onplayer){
+                player.transform.position = new Vector2(0,0);
+            }
+            justshot = true;
+            shooting = false;
+        }
+        if(justshot){
+            ashott += 20*Time.deltaTime;
+        }
+        if(ashott>= tilnext){
+            follow = true;
+            ashott = 0;
+            justshot = false;
+        }
         if (follow){
         transform.position = Vector2.MoveTowards(transform.position, player.position, followspeed*Time.deltaTime);
         }
