8d3eed1d294ecb611748051e7b92d84dcb87bc29
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-04-06 11:55:14 -0700

    more ui

--- lion2/Assets/code/start.cs
+++ lion2/Assets/code/start.cs
@@ -1,10 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class start : MonoBehaviour
 {
     public void playgame(){
-        Debug.Log("go go go");
+        SceneManager.LoadScene("level0");
+    }
+    public void level1(){
+        SceneManager.LoadScene("level1");
+    }
+    public void level2(){
+        SceneManager.LoadScene("level2");
+    }
+    public void level3(){
+        SceneManager.LoadScene("level3");
+    }
+    public void level4(){
+        SceneManager.LoadScene("level4");
+    }
+    public void level5(){
+        SceneManager.LoadScene("level5");
+    }
+    public void level6(){
+        SceneManager.LoadScene("level6");
+    }
+    public void level7(){
+        SceneManager.LoadScene("level7");
+    }
+    public void level8(){
+        SceneManager.LoadScene("level8");
     }
 }
