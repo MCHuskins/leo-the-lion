87ca74f8dcef8d1f877ac31fcf540eaa142f3190
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-04-06 11:24:08 -0700

    ui

---
+++ lion2/Assets/code/start.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    public void playgame(){
+        Debug.Log("go go go");
+    }
+}
