4b7f272adc73c8ad5e04f6ada4512f611ed1a13f
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-04-15 09:47:20 -0700

    add l one that leo made

---
+++ lion2/Assets/gdetection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gdetection : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    
+
+    private bool movingRight = true;
+
+    public Transform Gdetect;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        
+        RaycastHit2D ginfo = Physics2D.Raycast(Gdetect.position, Vector2.down, distance);
+
+        if (ginfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+
+            } else
+            {
+
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+
+            }
+        }
+    }
+
+}
---
+++ lion2/Assets/wdetection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wdetection : MonoBehaviour
+{
+    
+    public float distance;
+
+    private bool movingRight = true;
+
+    public Transform wdetect;
+
+    private void Update()
+    {
+
+        
+        RaycastHit2D winfo = Physics2D.Raycast(wdetect.position, Vector2.down, distance);
+
+        if (winfo.collider == true)
+        {
+            if (movingRight == true)
+            {
+
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+
+            }
+            else
+            {
+
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+
+            }
+        }
+    }
+}
